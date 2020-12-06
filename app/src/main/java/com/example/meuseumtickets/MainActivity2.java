d392ed8f0ba6c45603172b7906cf95eff67fc5f4
Maudiel Romero <41795758+MaudielR@users.noreply.github.com>
2020-12-06 00:13:20 -0500

    Updated MainActivity to only use MainActivity2 and send images through.

--- app/src/main/java/com/example/meuseumtickets/MainActivity2.java
+++ app/src/main/java/com/example/meuseumtickets/MainActivity2.java
@@ -1,14 +1,24 @@
 package com.example.meuseumtickets;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity2 extends AppCompatActivity {
+    private ImageView MuseumPhoto;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        MuseumPhoto = (ImageView) findViewById(R.id.MuseumPhoto);
+        MuseumPhoto.setImageResource(getIntent().getIntExtra(MainActivity.IMAGE, R.drawable.spy_museum));
+
     }
+
 }
